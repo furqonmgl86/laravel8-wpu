f5db94e654725f8a1b42d8dc6eeebb659e8039f2
furqonmgl86 <fur_qon9462@yahoo.co.id>
2023-03-02 22:40:49 +0700

    Belajar templating Engine

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return "Hallo Furqon";
-    // return view('welcome');
+    return view('home', [
+        "title" => "Home"
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "title" => "About",
+        "name" => "Muhammad Furqon",
+        "email" => "fareqon9282@gmail.com",
+        "image" => "frq.jpeg"
+    ]);
+});
+Route::get('/blog', function () {
+    return view('posts', [
+        "title" => "Posts"
+    ]);
 });
