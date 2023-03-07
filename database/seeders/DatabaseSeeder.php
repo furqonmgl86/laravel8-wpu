aea5e361758b2cb45d88139837aff0524543b481
furqonmgl86 <fur_qon9462@yahoo.co.id>
2023-03-07 14:20:31 +0700

    9. Database Seedeer

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +21,71 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name' => "Muhammad Furqon",
+            'email' => 'furqon827@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+        User::create([
+            'name' => "Fitri Fauziah",
+            'email' => 'fauziah@gmail.com',
+            'password' => bcrypt('123457')
+        ]);
+        User::create([
+            'name' => "Ihsan Maulana",
+            'email' => 'fisasas@gmail.com',
+            'password' => bcrypt('123457232')
+        ]);
+
+        Category::create([
+            'name' => "Web Programming",
+            'slug' => 'web-progamming'
+        ]);
+        Category::create([
+            'name' => "Web Design",
+            'slug' => 'web-design'
+        ]);
+        Category::create([
+            'name' => "Personal",
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'category_id' => 1,
+            'user_id' => 1,
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Anak polah bapak kepradah. Agaknya peribahasa Jawa ini cocok betul dengan apa yang dialami Rafael Alun Trisambodo selepas ulah anaknya, Mario Dandy Satriyo, yang menganiaya seorang pelajar',
+            'body' => '<p>Anak polah bapak kepradah. Agaknya peribahasa Jawa ini cocok betul dengan apa yang dialami Rafael Alun Trisambodo selepas ulah anaknya, Mario Dandy Satriyo, yang menganiaya seorang pelajar bernama Cristalino David Ozora. Buntut Mario yang menjadi tersangka penganiayaan, harta kekayaan Rafael Alun menjadi sorotan. Waktu berlalu dan kini Mario dijerat dengan pasal yang lebih berat, sedangkan Rafael Alun masih terus ditelusuri awal mula hartanya yang disinyalir tak masuk akal.</p>'
+        ]);
+          
+          Post::create([
+            'title' => 'Judul Kedua',
+            'category_id' => 2,
+            'user_id' => 2,
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Jerman tengah membangun jembatan gantung di atas ketinggian 120 meter. Jembatan ini diharapkan menjadi daya tarik wisata utama di "Hutan Hitam" Jakarta',
+            'body' => '<p>Jerman tengah membangun jembatan gantung di atas ketinggian 120 meter. Jembatan ini diharapkan menjadi daya tarik wisata utama di "Hutan Hitam" Jakarta - MOTUL FIM Superbike World Championship (WSBK atau WorldSBK) Indonesian Round 2023 selesai digelar. Ajang ini diselenggarakan di Pertamina Mandalika International Circuit, Lombok, Nusa Tenggara Barat (NTB) pada 3-5 Maret 2023.PT Pengembangan Pariwisata Indonesia (Persero)/Indonesia Tourism Development Co</p>'
+          ]);
+          
+          Post::create([
+            'title' => 'Judul Ketiga',
+            'category_id' => 3,
+            'user_id' => 1,
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Personal ini tengah membangun jembatan gantung di atas ketinggian 120 meter. Jembatan ini diharapkan menjadi daya tarik wisata utama di "Hutan Hitam" Jakarta',
+            'body' => '<p>Personal ini tengah membangun jembatan gantung di atas ketinggian 120 meter. Jembatan ini diharapkan menjadi daya tarik wisata utama di "Hutan Hitam" Jakarta - MOTUL FIM Superbike World Championship (WSBK atau WorldSBK) Indonesian Round 2023 selesai digelar. Ajang ini diselenggarakan di Pertamina Mandalika International Circuit, Lombok, Nusa Tenggara Barat (NTB) pada 3-5 Maret 2023.PT Pengembangan Pariwisata Indonesia (Persero)/Indonesia Tourism Development Co</p>'
+          ]);
+          Post::create([
+            'title' => 'Judul Keempat',
+            'category_id' => 3,
+            'user_id' => 3,
+            'slug' => 'judul-keempat',
+            'excerpt' => 'Personal ini tengah membangun jembatan gantung di atas ketinggian 120 meter. Jembatan ini diharapkan menjadi daya tarik wisata utama di "Hutan Hitam" Jakarta',
+            'body' => '<p>Personal ini tengah membangun jembatan gantung di atas ketinggian 120 meter. Jembatan ini diharapkan menjadi daya tarik wisata utama di "Hutan Hitam" Jakarta - MOTUL FIM Superbike World Championship (WSBK atau WorldSBK) Indonesian Round 2023 selesai digelar. Ajang ini diselenggarakan di Pertamina Mandalika International Circuit, Lombok, Nusa Tenggara Barat (NTB) pada 3-5 Maret 2023.PT Pengembangan Pariwisata Indonesia (Persero)/Indonesia Tourism Development Co</p>'
+          ]);
+          
     }
+
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,13 +6,15 @@
 
 <h1>Ini Halaman Blog Posts</h1>
 @foreach ($posts as $post)
-    <article class="mb-3">
+    <article class="mb-5 border-bottom pb-4">
       <h2>
         {{-- <a href="/posts/{{$post["id"]}}">{{$post["title"]}}</a> --}}
-        <a href="/posts/{{$post->slug}}">{{$post->title}}</a>
+        <a href="/posts/{{$post->slug}}" class="text-decoration-none">{{$post->title}}</a>
       </h2>
+      <p>By <a href="" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{$post->category->slug}}" class="text-decoration-none">{{ $post->category->name}}</a> </p>
       {{-- <h5>By : {{$post["author"]}}</h5> --}}
       <p>{{$post->excerpt}}</p>
+      <a href="/posts/{{$post->slug}}" class="text-decoration-none">Read More.....</a>
     </article>
 @endforeach
 @endsection
