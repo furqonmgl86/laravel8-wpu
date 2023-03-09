bf2d65c7c0744cfedf46d1174c1443506adc43e9
furqonmgl86 <fur_qon9462@yahoo.co.id>
2023-03-09 14:13:26 +0700

    Membuat View Register dan Login

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,8 +5,13 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="/css/bootstrap.css">
-  <link rel="stylesheet" href="/css/style.css">
+  {{-- <link rel="stylesheet" href="/css/style.css"> --}}
   <title>WPU BLOG || {{$title}}</title>
+  {{-- Bootsrap Icon --}}
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+
+  {{-- My STYLE --}}
+  <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
  @include('partials.navbar')
