630f95b88bca66f94622f0d6654a8452c4bdcba8
furqonmgl86 <fur_qon9462@yahoo.co.id>
2023-03-11 13:02:24 +0700

    23. Authorization Gate Middleware

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +23,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFive();
+        Gate::define('admin', function(User $user){
+           return  $user->is_admin;
+        });
     }
 }
+
+
